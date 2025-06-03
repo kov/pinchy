7d6878860214ad7afc9332686827614d7b17e10f
Gustavo Noronha Silva <gustavo@noronha.dev.br>
2025-06-03 08:25:22 -0300

    Move poll_bits_to_strs to util module
    This big but irrelevant most of the time function at the top of
    events.rs was driving me a bit mad. Hide it into a util module.

---
+++ pinchy/src/util.rs
@@ -0,0 +1,49 @@
+pub fn poll_bits_to_strs(event: &i16) -> Vec<&'static str> {
+    let mut strs = vec![];
+
+    if event & libc::POLLIN != 0 {
+        strs.push("POLLIN");
+    }
+
+    if event & libc::POLLPRI != 0 {
+        strs.push("POLLPRI");
+    }
+
+    if event & libc::POLLOUT != 0 {
+        strs.push("POLLOUT");
+    }
+
+    if event & libc::POLLRDHUP != 0 {
+        strs.push("POLLRDHUP");
+    }
+
+    if event & libc::POLLERR != 0 {
+        strs.push("POLLERR");
+    }
+
+    if event & libc::POLLHUP != 0 {
+        strs.push("POLLHUP");
+    }
+
+    if event & libc::POLLNVAL != 0 {
+        strs.push("POLLNVAL");
+    }
+
+    if event & libc::POLLRDNORM != 0 {
+        strs.push("POLLRDNORM");
+    }
+
+    if event & libc::POLLRDBAND != 0 {
+        strs.push("POLLRDBAND");
+    }
+
+    if event & libc::POLLWRNORM != 0 {
+        strs.push("POLLWRNORM");
+    }
+
+    if event & libc::POLLWRBAND != 0 {
+        strs.push("POLLWRBAND");
+    }
+
+    strs
+}

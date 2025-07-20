ad810b540de59be19b21f31e549afe3148370268
Gustavo Noronha Silva <gustavo@noronha.dev.br>
2025-07-20 20:44:19 -0300

    Add SPDX headers to test files

--- pinchy/src/tests/system.rs
+++ pinchy/src/tests/system.rs
@@ -1,3 +1,6 @@
+// SPDX-License-Identifier: MIT OR Apache-2.0
+// Copyright (c) 2025 Gustavo Noronha Silva <gustavo@noronha.dev.br>
+
 #[tokio::test]
 async fn parse_rt_sigreturn() {
     use pinchy_common::RtSigreturnData;
